ba0c2eaaa433b65e5469bae271d3e2549397ccb6
fadleyy <user.fadli3001@gmail.com>
2023-11-26 18:58:33 +0700

    mempelajari basic router, requets, response:hankey:

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
 const express = require('express');
 
 const app = express();
+const router = express.Router();
 
-app.use(() => {
-    console.log('Hello server');
+router.get('/author', (req, res) => {
+    res.json({
+        name: "fadli",
+        email: "fadli@gaga.go"
+    })
+    console.log({
+        reqUrl: req.originalUrl,
+        reqMethod: req.method
+    })
 })
+
+app.use('/', router)
 
 app.listen(4000)
